51869fd2b03820c82a11788ac05a37fc93f1fa33
Mounir <hamzaouimounirlfig@gmail.com>
2017-03-01 11:56:24 +0100

    Facebook Login
    Tested Oauth Facebook showing user information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
 import { TabsPage } from '../pages/tabs/tabs';
 
 
@@ -15,6 +14,7 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,15 +1,60 @@
+import { Response } from '@angular/http';
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, ToastController } from 'ionic-angular';
+import { Facebook, NativeStorage } from 'ionic-native';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
+   output="Stand by";
+   
+   FB_APP_ID: number = 1368002846591785;
+  constructor(public navCtrl: NavController,public toastCtrl: ToastController) {
+        Facebook.browserInit(this.FB_APP_ID);
   }
-
+  // 
+  connectFacebook(){
+    //Connexion à Facebook
+    let permissions = new Array();
+    //let nav = this.navCtrl;
+    let env =this;
+    //the permissions your facebook app needs from the user
+    permissions = ["public_profile"];
+    Facebook.login(permissions)
+    .then(function(response){
+      let userId = response.authResponse.userID;
+      let params = new Array();
+      
+      //Getting name and gender properties
+      Facebook.api("/me?fields=name,gender", params)
+      .then(function(user) {
+       
+        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+         alert(JSON.stringify(user));
+          env.output=JSON.stringify(user);
+        //now we have the users info, let's save it in the NativeStorage
+        NativeStorage.setItem('user',
+        {
+          name: user.name,
+          gender: user.gender,
+          picture: user.picture
+        })
+        .then(function(){
+          //nav.push(HomePage);
+          this.log(user);
+         
+        }, function (error) {
+          console.log(error);
+        });
+      })
+      
+      
+    }, function(error){
+      console.log(error);
+    });
+  }
+   
+  
 }
